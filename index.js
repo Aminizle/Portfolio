1dd60dff905264d9e32148b968b6fc6a3fc8a0ca
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2022-04-23 22:02:25 -0400

    added my projects and faders

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
+const faders = document.querySelectorAll('.services')
 
+
+//Nav menu toggle 
 navToggle.addEventListener('click', ()=> {
     document.body.classList.toggle('nav-open')
 })
@@ -9,4 +12,33 @@ navLinks.forEach(link => {
     link.addEventListener('click', ()=> {
         document.body.classList.remove('nav-open');
     })
+})
+
+
+
+
+//Fade in 
+const appearOptions = {
+    threshold: 1, // Only when element is shown in full does it fade in
+    rootMargin: "0px 0px -75px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver //Crossing from one section to the next triggers this
+(function(
+    entries,
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {  //makes sure only those that are intersecting are triggered
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+},
+appearOptions);
+
+faders.forEach(faders => {
+    appearOnScroll.observe(faders);
 })
